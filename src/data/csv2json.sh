44fa8e4d7b2dac9f40a28e1121b932333fb53470
nagar@wsl2 <nagaratokuma@gmial.com>
2025-01-21 13:19:21 +0900

    [update]　問題をCSVファイルから読みとように変更

---
+++ src/data/csv2json.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+csvFilePath="childrenQuestion.csv" # CSVファイルのパスを指定
+jsonFilePath="childrenQuestion.json"
+
+# CSVファイルを読み込み、JSON形式に変換
+jsonData=$(awk -F, '
+BEGIN {
+    print "["
+}
+NR > 1 {
+    if (NR > 2) {
+        printf ",\n"
+    }
+    printf "{\n"
+    printf "  \"id\": %d,\n", NR-1
+    printf "  \"text\": \"%s\",\n", $1
+    printf "  \"options\": [\"%s\", \"%s\", \"%s\", \"%s\"],\n", $2, $3, $4, $5
+    printf "  \"correctAnswer\": %d,\n", $6 - 1
+    printf "  \"explanation\": \"%s\",\n", $7
+    printf "  \"explanationImage\": \"%s\"\n", $8
+    printf "}"
+}
+END {
+    print "\n]"
+}' "$csvFilePath" | tr -d '\r')
+
+# JSONデータをファイルに出力
+echo "$jsonData" > "$jsonFilePath"
+
+echo "JSON data created in $jsonFilePath"
+
+# 大人向けの問題
+csvFilePath="adultQuestion.csv" # CSVファイルのパスを指定
+jsonFilePath="adultQuestion.json"
+
+# CSVファイルを読み込み、JSON形式に変換
+jsonData=$(awk -F, '
+BEGIN {
+    print "["
+}
+NR > 1 {
+    if (NR > 2) {
+        printf ",\n"
+    }
+    printf "{\n"
+    printf "  \"id\": %d,\n", NR-1
+    printf "  \"text\": \"%s\",\n", $1
+    printf "  \"options\": [\"%s\", \"%s\", \"%s\", \"%s\"],\n", $2, $3, $4, $5
+    printf "  \"correctAnswer\": %d,\n", $6 - 1
+    printf "  \"explanation\": \"%s\",\n", $7
+    printf "  \"explanationImage\": \"%s\"\n", $8
+    printf "}"
+}
+END {
+    print "\n]"
+}' "$csvFilePath" | tr -d '\r')
+
+# JSONデータをファイルに出力
+echo "$jsonData" > "$jsonFilePath"
+
+echo "JSON data created in $jsonFilePath"
