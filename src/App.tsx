1769fe1db98a259a9de17fe7ec6d23a87bf1fc06
nagar@wsl2 <nagaratokuma@gmial.com>
2025-04-11 23:03:35 +0900

    Reactのインポートから不要な部分を削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { QuestionCard } from './components/QuestionCard';
 import { ProgressBar } from './components/ProgressBar';
 import { ResultCard } from './components/ResultCard';
